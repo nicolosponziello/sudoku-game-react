31c1da28906fad84ada542f54e441cbf249fbc01
nicolosponziello <nicolosponziello@gmail.com>
2020-08-17 11:01:15 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import "./components/Board";
 import Board from './components/Board';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Board boardSize={3} />
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from "react";
+
+export const Header: FC = () => {
+
+    return (
+        <div className="header">
+            <span className="title">SUDOKU</span>
+        </div>
+    )
+}
